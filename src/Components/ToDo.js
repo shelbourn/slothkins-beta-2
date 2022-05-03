e13814fe665019cb36edf1aef37f2410fe4b852c
Matthew Shelbourn <matthew@shelbourn.io>
2022-05-03 03:23:58 -0500

    SLOTH: Updating to-do
    NOT MANY TO-DO'S LEFT! :tada:

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -1,15 +1,15 @@
 //[ ] Remove all comments and console logs
-//[ ] Clean up CSS / CSS Classes
-//[ ] Clean up UI/UX
+//[x] Clean up CSS / CSS Classes
+//[x] Clean up UI/UX
 //[x] Add handler to calculate Log Reg Prob based on Open price *** DONE ***
-//[ ] Add handler / fields to add full DB entry for specific currency based on open price,
+//[x] Add handler / fields to add full DB entry for specific currency based on open price,
 // close price, calculated MAV,
-//[ ] Authentication
-//[ ] Break K-Means charts into their own component
-//[ ] Add button/handler for fetching log reg data
+//[x] Authentication
+//[x] Break K-Means charts into their own component
+//[x] Add button/handler for fetching log reg data
 //[x] In LogRegProbFields.js -- calculate whether there is a BUY signal and
 // what the logRegProb is for NEXT DATE give the OPENING PRICE provided *** DONE ***
-//[ ] Different data point (open, open-open, 10-day MAV, etc) to LogRegChart.js
+//[x] Different data point (open, open-open, 10-day MAV, etc) to LogRegChart.js
 //[x] Change Tooltip style for all LogRegChart.js charts
 //[x] Fix conditional rendering related to CrytoStore.loaded.logRegFields
 // and date property of CryptoStore.logRegUsableData -- currently it will change the
@@ -20,5 +20,7 @@
 //[ ] Add code comments
 //[ ] Good site for historical currency data: https://coinmarketcap.com/currencies/bitcoin/historical-data/
 //[ ] Another good site: https://www.tradingview.com/chart/?symbol=CRYPTOCAP%3ATOTAL
-//[ ] Change loading prop for KMeans component <LoadingButton />
-//[ ] Add site header with nav
+//[x] Change loading prop for KMeans component <LoadingButton />
+//[x] Add site header with nav
+//[ ] Add helper text to LogRegProbFields.js
+//[ ] Add helper text to KMeansChart.js
