b9dcbcc3fcb0272d89dfadbc899acece768da95a
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2022-10-15 00:05:21 -0500

    Fixing routing issue that borked deployment

--- src/Router/Routing.tsx
+++ src/Router/Routing.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Landing from '../Landing.tsx';
+import Landing from '../Landing';
 import KMeansContainer from '../Containers/KMeansContainer';
 import LogRegressionContainer from '../Containers/LogRegressionContainer';
 import LogRegProbFields from '../Components/LogRegProbFields';
@@ -12,30 +12,26 @@ import MainHeader from '../Common/MainHeader';
  * Declares all app routes and handles routing
  */
 
-const Routing = () => {
+const Routing = (): JSX.Element => {
     return (
         <Router>
             <MainHeader />
             <Routes>
-                <Route path="/" exact element={<Landing />} />
+                <Route path="/" element={<Landing />} />
                 <Route
                     path="/k-means-clustering"
-                    exact
                     element={<KMeansContainer />}
                 />
                 <Route
                     path="/logistic-regression"
-                    exact
                     element={<LogRegressionContainer />}
                 />
                 <Route
                     path="/next-day-buy-signal-prediction"
-                    exact
                     element={<LogRegProbFields />}
                 />
                 <Route
                     path="/add-crypto-price-data"
-                    exact
                     element={<AddCryptoPriceData />}
                 />
             </Routes>
