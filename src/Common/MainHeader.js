42342323d4ac4ed55b37b02f7dc718dc9622b8db
Matthew Shelbourn <11466465+shelbourn@users.noreply.github.com>
2023-05-08 22:22:37 -0500

    Reverting all ts conversions

--- src/Common/MainHeader.js
+++ src/Common/MainHeader.js
@@ -16,7 +16,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import { useStore } from '../Stores/StoreFunctions';
 import Sloth from '../Assets/sloth-icon-header.png';
 
-const MainHeader = (): JSX.Element => {
+const MainHeader = () => {
     const { CryptoStore } = useStore();
 
     const [anchorElNav, setAnchorElNav] = React.useState(null);
